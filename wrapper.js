9c59f4bccdca6110388b8e2df2bde70f1f2c34e4
Matthias Zaunseder <zaunseder@outlook.com>
2020-11-12 15:07:50 +0100

    Remove wrapper and slim down package.json

--- wrapper.js
+++
@@ -1,6 +0,0 @@
-// Wrapper for ES "import"
-// @see https://nodejs.org/api/packages.html#packages_approach_1_use_an_es_module_wrapper
-
-import ImageBlobReduce from './index.js';
-
-export default ImageBlobReduce;
