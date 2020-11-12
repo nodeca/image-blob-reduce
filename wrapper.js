60eb68542294e51f16960e509b13b082d213d13f
Matthias Zaunseder <zaunseder@outlook.com>
2020-11-12 13:53:45 +0100

    Add module wrapper for CommonJS

---
+++ wrapper.js
@@ -0,0 +1,6 @@
+// Wrapper for ES "import"
+// @see https://nodejs.org/api/packages.html#packages_approach_1_use_an_es_module_wrapper
+
+import ImageBlobReduce from './index.js';
+
+export default ImageBlobReduce;
